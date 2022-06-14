1ac446ec288a8a427d8664bfee94a468d7999c20
swaaz <swaasthik.shetty07@gmail.com>
2022-06-14 13:05:28 +0530

    Updated path

--- app.py
+++ app.py
@@ -1,51 +1,54 @@
+from werkzeug.utils import secure_filename
+from flask import Flask, render_template, request
+from prod_gen_spectrograph import gen_spec_segments
+from prod_split_audio import split_audio
+from keras.preprocessing import image
+from pprint import pprint
+import numpy as np
 import os
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('Agg')
-import numpy as np
-from pprint import pprint
 
-from keras.preprocessing import image
 
-from prod_split_audio import split_audio
-from prod_gen_spectrograph import gen_spec_segments
-
-from flask import Flask, render_template, request
-from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = "./"
 
-model = tf.keras.models.load_model('my_model.h5')
+model = tf.keras.models.load_model('../Model/my_model.h5')
+
 
 @app.route('/upload')
 def upload_file():
-   return render_template('upload.html')
-    
-@app.route('/uploader', methods = ['GET', 'POST'])
+    return render_template('upload.html')
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
 def upload_file_two():
     if request.method == 'POST':
         f = request.files['file']
         f.save(secure_filename(f.filename))
-        
+
         split_audio('./'+secure_filename(f.filename), 2)
         gen_spec_segments('./'+secure_filename(f.filename))
-        
+
         prediction = {}
-        file_path = "./data/prod/spectrogram/segments/"+secure_filename(f.filename).split(".")[0]
-        
+        file_path = "./data/prod/spectrogram/segments/" + \
+            secure_filename(f.filename).split(".")[0]
+
         print(file_path)
-        
+
         for file in os.listdir(file_path):
-        # print(file)
-            img=image.load_img(os.path.join(file_path, file), target_size=(310, 154, 3))
-            
-            x=image.img_to_array(img)
+            # print(file)
+            img = image.load_img(os.path.join(
+                file_path, file), target_size=(310, 154, 3))
+
+            x = image.img_to_array(img)
             x /= 255
-            x=np.expand_dims(x, axis=0)
+            x = np.expand_dims(x, axis=0)
             images = np.vstack([x])
-            
+
             classes = model.predict(images, batch_size=10)
 
             # print(classes[0])
@@ -65,13 +68,9 @@ def upload_file_two():
                 prediction[time_stamp] = "regular"
 
         pprint(prediction)
-        
+
         return prediction
-                
-        
 
 
-    
-        
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug=True)
