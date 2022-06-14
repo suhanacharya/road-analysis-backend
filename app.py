3b6b1b6055652297769bb9919464c30055a4ff4c
suhanacharya <suhan.acharya1@gmail.com>
2022-06-14 13:04:06 +0530

    Updated backend

--- app.py
+++ app.py
@@ -5,19 +5,56 @@ import matplotlib
 matplotlib.use('Agg')
 import numpy as np
 from pprint import pprint
+import requests
 
 from keras.preprocessing import image
 
 from prod_split_audio import split_audio
 from prod_gen_spectrograph import gen_spec_segments
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, render_template_string
 from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = "./"
 
 model = tf.keras.models.load_model('my_model.h5')
+
+def generate_result(prediction):
+    color_map = {
+    "regular": "lightgreen",
+    "bump": "red",
+    "gear-change": "orange",
+    "horn": "yellow"
+    }
+
+    html = """
+    <html>
+        <body>
+            <h1>Prediction</h1>
+            <table>
+                <tr>
+                
+    """
+    for i in range(0, len(prediction)*2, 2):
+        td = ""
+        time_stamp = str(float(i)) + "-" + str(float(i+2))
+        color = color_map[prediction[time_stamp]]
+        td += f"<td style='width:100%;background-color:{color}'>{time_stamp}</td>"
+        print(time_stamp, prediction[time_stamp])
+        html += td
+        
+    html += """</tr></table>
+    <br>
+    <table>
+        <tr><td style='background-color:lightgreen'>Regular</td></tr>
+        <tr><td style='background-color:red'>Bump</td></tr>
+        <tr><td style='background-color:orange'>Gear-change</td></tr>
+        <tr><td style='background-color:yellow'>Horn</td></tr>
+    </table>
+    </body></html>"""
+
+    return html
 
 @app.route('/upload')
 def upload_file():
@@ -66,10 +103,68 @@ def upload_file_two():
 
         pprint(prediction)
         
-        return prediction
-                
+        html = generate_result(prediction)
         
+        # return prediction
+        return render_template_string(html)
+    
+@app.route('/analyze', methods = ['POST'])
+def analyze():
+    if request.method == 'POST':
+        f = request.form["url"]
+        print(f)
+        
+        r = requests.get(f)
+        # fname = os.path.basename(f).split("?")[0]
+        fname = f.split("/")[-1]
+        print(fname)
+        with open(fname, 'wb') as f:
+            f.write(r.content)
+                        
+        split_audio('./' + fname, 2)
+        gen_spec_segments('./' + fname)
+        
+        prediction = {}
+        file_path = "./data/prod/spectrogram/segments/"+fname.split(".")[0]
+        
+        print(file_path)
+        
+        for file in os.listdir(file_path):
+        # print(file)
+            img=image.load_img(os.path.join(file_path, file), target_size=(310, 154, 3))
+            
+            x=image.img_to_array(img)
+            x /= 255
+            x=np.expand_dims(x, axis=0)
+            images = np.vstack([x])
+            
+            classes = model.predict(images, batch_size=10)
 
+            # print(classes[0])
+            time_stamp = file.split("_")[1].rstrip(".png")
+            maxi = max(classes[0])
+            if classes[0][0] == maxi:
+                # print(time_stamp, "bump")
+                prediction[time_stamp] = "bump"
+            elif classes[0][1] == maxi:
+                # print(time_stamp, "gear-change")
+                prediction[time_stamp] = "gear-change"
+            elif classes[0][2] == maxi:
+                # print(time_stamp, "horn")
+                prediction[time_stamp] = "horn"
+            elif classes[0][3] == maxi:
+                # print(time_stamp, "regular")
+                prediction[time_stamp] = "regular"
+
+        # pprint(prediction)
+        
+        new_pred = {}
+        for i in range(0, len(prediction)*2, 2):
+            time_stamp = str(float(i)) + "-" + str(float(i+2))
+            new_pred[time_stamp] = prediction[time_stamp]
+            
+        pprint(new_pred)
+        return new_pred
 
     
         
